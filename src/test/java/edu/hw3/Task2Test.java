2c725d62140b1ae70e1df3e80fd3b374a85a023d
Ivan Simonov <ivansimonov7@yandex.ru>
2023-10-23 22:46:00 +0300

    HW3 1.2

--- src/test/java/edu/hw3/Task2Test.java
+++ src/test/java/edu/hw3/Task2Test.java
@@ -1,4 +1,63 @@
 package edu.hw3;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Task2Test {
+    @Test
+    @DisplayName("Проверка примера №1")
+    void test1() {
+        String actual = Task2.clusterize("()()()");
+        String expected = "[\"()\", \"()\", \"()\"]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка примера №2")
+    void test2() {
+        String actual = Task2.clusterize("((()))");
+        String expected = "[\"((()))\"]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка примера №3")
+    void test3() {
+        String actual = Task2.clusterize("((()))(())()()(()())");
+        String expected = "[\"((()))\", \"(())\", \"()\", \"()\", \"(()())\"]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка примера №4")
+    void test4() {
+        String actual = Task2.clusterize("((())())(()(()()))");
+        String expected = "[\"((())())\", \"(()(()()))\"]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка неверной строки №1")
+    void test5() {
+        String actual = Task2.clusterize("))((");
+        String expected = "[]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка неверной строки №2")
+    void test6() {
+        String actual = Task2.clusterize("((((");
+        String expected = "[]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка неверной строки №3")
+    void test7() {
+        String actual = Task2.clusterize("(()))");
+        String expected = "[]";
+        assertEquals(expected, actual);
+    }
 }
