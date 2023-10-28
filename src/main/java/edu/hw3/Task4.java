7a79d4d15adf945c10f7edbb76c0ec8452a842e8
Ivan Simonov <ivansimonov7@yandex.ru>
2023-10-28 17:16:37 +0300

    HW3 1.7

--- src/main/java/edu/hw3/Task4.java
+++ src/main/java/edu/hw3/Task4.java
@@ -1,108 +1,50 @@
 package edu.hw3;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-@SuppressWarnings({"RegexpSinglelineJava", "uncommentedmain", "magicnumber"})
+@SuppressWarnings({"RegexpSinglelineJava", "uncommentedmain"})
 public class Task4 {
     private Task4() {
 
     }
 
-    private final static int FOUR = 4;
-    private final static Integer DECADE = 10;
-    private final static Integer HALFHUNDRED = 50;
-    private final static Integer HUNDRED = 100;
-    private final static Integer HALFTHOUSAND = 500;
-    private final static Integer THOUSAND = 1000;
-
-    private static String letterM(Integer num) {
-        String m = "M";
-        return m.repeat(num);
-    }
-
-    private static String letterC(Integer num) {
-        if (num.equals(FOUR)) {
-            return "CD";
-        } else if (!num.equals(0) && num < FOUR) {
-            String m = "C";
-            return m.repeat(num);
-        } else {
-            return "";
-        }
-    }
-
-    private static String letterX(Integer num) {
-        if (num.equals(FOUR)) {
-            return "XL";
-        } else if (!num.equals(0) && num < FOUR) {
-            String x = "X";
-            return x.repeat(num);
-        } else {
-            return "";
-        }
-    }
-
-    private static String letterD(Integer num) {
-
-        if (num.equals(0)) {
-            return "";
-        } else if (num.equals(FOUR)) {
-            return "CM";
-        }
-        return "D";
-    }
-
-    private static String letterL(Integer num) {
-        if (num.equals(0)) {
-            return "";
-        } else if (num.equals(FOUR)) {
-            return "XC";
-        }
-        return "L";
-    }
-
-    private static String digits(Integer num) {
-        return switch (num) {
-            case 1 -> "I";
-            case 2 -> "II";
-            case 3 -> "III";
-            case 4 -> "IV";
-            case 5 -> "V";
-            case 6 -> "VI";
-            case 7 -> "VII";
-            case 8 -> "VIII";
-            case 9 -> "IX";
-            default -> "";
-        };
-    }
+    final static Integer MAX = 4000;
 
     public static String convertToRoman(Integer num) {
-        StringBuilder result = new StringBuilder("\"");
-        if (num > 0 && num < FOUR * THOUSAND) {
-            Integer thousands1 = num / THOUSAND;
-            result.append(letterM(thousands1));
-            Integer thousands2 = num % THOUSAND;
-
-            Integer halfThousand1 = thousands2 / HALFTHOUSAND;
-            result.append(letterD(halfThousand1));
-            Integer halfThousand2 = thousands2 % HALFTHOUSAND;
-
-            Integer hundreds1 = halfThousand2 / HUNDRED;
-            result.append(letterC(hundreds1));
-            Integer hundreds2 = halfThousand2 % HUNDRED;
-
-            Integer halfHundred1 = hundreds2 / HALFHUNDRED;
-            result.append(letterL(halfHundred1));
-            Integer halfHundred2 = hundreds2 % HALFHUNDRED;
-
-            Integer decades1 = halfHundred2 / DECADE;
-            result.append(letterX(decades1));
-            Integer decades2 = halfHundred2 % DECADE;
-
-            result.append(digits(decades2));
+        String result = "\"";
+        HashMap<String, Integer> hashMap = new LinkedHashMap<>() {{
+            put("M", 1000);
+            put("CM", 900);
+            put("D", 500);
+            put("CD", 400);
+            put("C", 100);
+            put("XC", 90);
+            put("L", 50);
+            put("XL", 40);
+            put("X", 10);
+            put("IX", 9);
+            put("V", 5);
+            put("IV", 4);
+            put("I", 1);
+        }};
+        String key;
+        Integer value;
+        while (num > 0 && num < MAX) {
+            for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                key = entry.getKey();
+                value = entry.getValue();
+                while (num >= value) {
+                    result += key;
+                    num -= value;
+                }
+            }
         }
-        result.append("\"");
-        return result.toString();
+        result += "\"";
+        return result;
     }
 
     public static void main(String[] args) {
