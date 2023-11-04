3f1707eb7b8a459213233d58081d6800b1bfefd8
Ivan Simonov <ivansimonov7@yandex.ru>
2023-11-04 19:42:43 +0300

    HW4 Style corrections

--- src/main/java/edu/hw4/Task8.java
+++ src/main/java/edu/hw4/Task8.java
@@ -9,7 +9,7 @@ public class Task8 {
 
     }
 
-    public static Optional<Animal> TheHeaviestAnimalBelowKHeight(List<Animal> animalList, Integer k) {
+    public static Optional<Animal> theHeaviestAnimalBelowKHeight(List<Animal> animalList, Integer k) {
         return animalList.stream().filter(i -> i.height() < k).max(Comparator.comparingInt(Animal::weight));
     }
 }
--- src/test/java/edu/hw4/Task8Test.java
+++ src/test/java/edu/hw4/Task8Test.java
@@ -16,7 +16,7 @@ public class Task8Test {
         list.add(new Animal("Eagle", Animal.Type.BIRD, Animal.Sex.M, 10, 7, 9, false));
         list.add(new Animal("Mr. Cat", Animal.Type.CAT, Animal.Sex.M, 1, 89, 17, true));
         list.add(new Animal("Maven", Animal.Type.BIRD, Animal.Sex.M, 111, 78, 19, true));
-        String actual = Task8.TheHeaviestAnimalBelowKHeight(list, 3).toString();
+        String actual = Task8.theHeaviestAnimalBelowKHeight(list, 3).toString();
         String expected = "Optional.empty";
         assertEquals(expected, actual);
     }
@@ -30,7 +30,7 @@ public class Task8Test {
         list.add(new Animal("Eagle", Animal.Type.BIRD, Animal.Sex.M, 10, 7, 9, false));
         list.add(new Animal("Mr. Cat", Animal.Type.CAT, Animal.Sex.M, 1, 89, 17, true));
         list.add(new Animal("Maven", Animal.Type.BIRD, Animal.Sex.M, 111, 78, 19, true));
-        String actual = Task8.TheHeaviestAnimalBelowKHeight(list, 50).toString();
+        String actual = Task8.theHeaviestAnimalBelowKHeight(list, 50).toString();
         String expected = "Optional[Majesty]";
         assertEquals(expected, actual);
     }
@@ -38,14 +38,14 @@ public class Task8Test {
     @Test
     @DisplayName("Проверка вывода самого тяжелого животного среди животных ниже k см на 'null' значении")
     void NullList() {
-        assertThrows(NullPointerException.class, () -> Task8.TheHeaviestAnimalBelowKHeight(null, 0).toString());
+        assertThrows(NullPointerException.class, () -> Task8.theHeaviestAnimalBelowKHeight(null, 0).toString());
     }
 
     @Test
     @DisplayName("Проверка вывода самого тяжелого животного среди животных ниже k см на пустом списке")
     void EmptyList() {
         ArrayList<Animal> list = new ArrayList<>();
-        String actual = Task8.TheHeaviestAnimalBelowKHeight(list, 0).toString();
+        String actual = Task8.theHeaviestAnimalBelowKHeight(list, 0).toString();
         String expected = "Optional.empty";
         assertEquals(expected, actual);
     }
