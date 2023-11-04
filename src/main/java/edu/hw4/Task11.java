234bddd37c893e2c670418020ef31181c641d5ae
Ivan Simonov <ivansimonov7@yandex.ru>
2023-11-04 19:43:01 +0300

    HW4 Style corrections

--- src/main/java/edu/hw4/Task11.java
+++ src/main/java/edu/hw4/Task11.java
@@ -6,7 +6,9 @@ public class Task11 {
     private Task11() {
 
     }
+
     final static int MIN_HEIGHT = 100;
+
     public static List<Animal> bitesAndHeightMoreThan100(List<Animal> animalList) {
         return animalList.stream().filter(i -> i.bites() && i.height() > MIN_HEIGHT).toList();
     }
